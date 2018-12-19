4e10980c390b35fbfb5dc5a08211d24bfbe5162b
Moimus <33897774+Moimus@users.noreply.github.com>
2018-12-19 12:50:30 +0100

    init

---
+++ GridManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+
+    public int xSize = 1;
+    public int ySize = 1;
+    public GameObject[] grid;
+    public GameObject[] pointCollection;
+
+	// Use this for initialization
+	void Start () {
+        grid = generateGrid(xSize, ySize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject[] generateGrid(int xSize, int ySize)
+    {
+        List<GameObject> field = new List<GameObject>();
+        for (int xn = 0; xn < xSize; xn++)
+        {
+            GameObject pointX = Instantiate(pointCollection[Random.Range(0, 2)]);
+            pointX.GetComponent<Point>().create(xn , 0);
+            field.Add(pointX);
+            for (int yn = 0; yn < ySize; yn++)
+            {
+                GameObject pointY = Instantiate(pointCollection[Random.Range(0, 2)]);
+                pointY.GetComponent<Point>().create(xn, yn);
+                field.Add(pointY);
+            }
+        }
+        GameObject[] fieldArray = new GameObject[field.Count];
+        fieldArray = field.ToArray();
+        return fieldArray;
+    }
+
+    public Point getPointByPosition(int x, int y)
+    {
+        Point result = null;
+
+        for (int n = 0; n < grid.Length; n++)
+        {
+            Point point = grid[n].GetComponent<Point>();
+            if (point.x == x && point.y == y)
+            {
+                result = point;
+            }
+        }
+
+        return result;
+    }
+
+    public Point[] getNeighbourPoints(int xPos, int yPos)
+    {
+        Point rootPos = this.getPointByPosition(xPos, yPos);
+        Point[] neighbours = new Point[]
+        {
+                this.getPointByPosition(xPos - 1, yPos - 1), // NW point
+				this.getPointByPosition(xPos, yPos - 1), //N point
+				this.getPointByPosition(xPos + 1, yPos - 1), //NE point
+				this.getPointByPosition(xPos - 1, yPos), //E point
+				this.getPointByPosition(xPos + 1, yPos), //W point
+				this.getPointByPosition(xPos - 1, yPos + 1), //SE point
+				this.getPointByPosition(xPos, yPos + 1), //S point
+				this.getPointByPosition(xPos + 1, yPos + 1) //SW point
+				
+		};
+
+        return neighbours;
+    }
+
+    public void debugGetPoint()
+    {
+        Debug.Log(getPointByPosition(2, 4).ToString());
+        Point[] neighbours = getNeighbourPoints(2, 4);
+        Debug.Log(neighbours.Length);
+        for (int n = 0; n < neighbours.Length; n++)
+        {
+            try
+            {
+                Debug.Log(neighbours[n].gameObject.ToString());
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Point : MonoBehaviour
+{
+    public bool passable = true;
+    public int x = 0;
+    public int y = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //just override/overload this
+    public void create(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        Vector3 pos = new Vector3(x, 0, y);
+        gameObject.transform.position = pos;
+    }
+
+    public override string ToString()
+    {
+        string str = this.GetType().ToString() + "{ TransformX:" + gameObject.transform.position.x + " TransformY:" + gameObject.transform.position.z + " X:" + x + " Y:" + y + " }";
+        return str;
+    }
+}
